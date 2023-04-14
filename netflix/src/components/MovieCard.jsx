49d9affe5378a5ca1b1aebdb2ce201d6fa2ba20f
whdgjs7300 <112137464+whdgjs7300@users.noreply.github.com>
2023-04-15 00:10:07 +0900

    4/15 cardCss

--- netflix/src/components/MovieCard.jsx
+++ netflix/src/components/MovieCard.jsx
@@ -1,3 +1,4 @@
+import Badge from 'react-bootstrap/Badge';
 
 const MovieCard = ({item}) => {
     
@@ -10,7 +11,14 @@ const MovieCard = ({item}) => {
         backgroundRepeat: "no-repeat",
         backgroundSize: "cover"
         }}>
-            
+        <div className='overlay'>
+            <h1 className="card_title">{item.title}</h1>
+            <div>{item.genre_ids.map(id=><Badge bg="danger">id</Badge>)}</div>
+            <div>
+                <span>{item.vote_average}</span>
+                <span>{item.adult ? "청불" : "Under 18"}</span>
+            </div>
+        </div>
             
         </div>
     );
