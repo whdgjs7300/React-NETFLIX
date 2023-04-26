f4f434ea6d9971d4f2de11394880ef8e8c2eae64
whdgjs7300 <112137464+whdgjs7300@users.noreply.github.com>
2023-04-26 23:55:05 +0900

    4/26 ModalSet

--- netflix/src/components/Modals.jsx
+++ netflix/src/components/Modals.jsx
@@ -1,30 +1,44 @@
 import Modal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
 import { useSelector } from 'react-redux';
+import YouTube, { YouTubeProps } from 'react-youtube';
 
 const Modals = ({setModal}) => {
 
-    const {videoList} = useSelector(state=> state.movie)
+    const {videoList, detailList} = useSelector(state=> state.movie)
     console.log(videoList)
+
+    const onPlayerReady: YouTubeProps['onReady'] = (event) => {
+        // access to player in all event handlers via event.target
+        event.target.pauseVideo();
+    }
+    const opts: YouTubeProps['opts'] = {
+        height: '390',
+        width: '640',
+        playerVars: {
+          // https://developers.google.com/youtube/player_parameters
+        autoplay: 1,
+        },
+    };
 
     return (  
         <div
         className="modal show"
-        style={{ display: 'block', position: 'initial',      
-    }} onClick={()=>{
-        setModal(false)
+        style={{ display: 'block', position: 'initial',     
     }}
         >
-        <Modal.Dialog >
-            <Modal.Header closeButton>
-            <Modal.Title>Modal title</Modal.Title>
+        <Modal.Dialog style={{backgroundColor : 'black'}} >
+            <Modal.Header closeButton  onClick={()=>{
+        setModal(false)
+    }}>
+            <Modal.Title >{detailList.title}</Modal.Title>
             </Modal.Header>
 
             <Modal.Body>
-            <p>Modal body text goes here.</p>
+            <YouTube videoId={videoList.results[1].key} opts={opts} onReady={onPlayerReady} />;
             </Modal.Body>
 
-            <Modal.Footer>
+            <Modal.Footer >
 
             
         </Modal.Footer>
